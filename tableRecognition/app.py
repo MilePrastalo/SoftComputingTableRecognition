f13718d8ea244fcfb1a06f196cca71301cb37b95
MilePrastalo <mile.prastalo97@gmail.com>
2020-02-11 13:46:10 +0100

    created method for endpoint

--- tableRecognition/app.py
+++ tableRecognition/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import base64
+
+from service.recogniseTable import recogniseTableFromImage
 
 app = Flask(__name__)
 
@@ -7,6 +10,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/image', methods=["POST"])
+def upload_image():
+    picture = Flask.request.get_json()
+    pictureBase64 = picture['pictureData']
+    imgdata = base64.b64decode(pictureBase64)
+    table_image = recogniseTableFromImage(imgdata)
 
 if __name__ == '__main__':
     app.run()
---
+++ tableRecognition/service/recogniseTable.py
@@ -0,0 +1,3 @@
+def recogniseTableFromImage(img_data):
+    image = None
+    return image
